7b1def0252e2ca9070fbd92d8c5757875d43ff1c
NavasMuhammed <navas3444a3@gmail.com>
2023-05-16 16:40:44 +0530

    feat: tab navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import RootLayout from "./layout";
 import HomeLayout from "./layouts/homeLayout";
 import "./globals.scss"
 import { Progress } from 'antd';
+import { Tabs } from 'antd';
 
 export default function Home() {
+
   return (
     <RootLayout>
       <HomeLayout>
@@ -17,8 +19,27 @@ export default function Home() {
                 <span>3%</span>
               </section>
               <div>
-                <Progress percent={10} trailColor="#D3D7D7" steps={17} strokeColor="#00B686" showInfo={false} size={10} />
+                <Progress percent={10} trailColor="#D3D7D7" steps={17} strokeColor="#00B686" showInfo={false} size={11} />
               </div>
+            </div>
+          </div>
+          <div className="project-content-wrapper">
+            <div>
+
+              <Tabs type="card" tabBarGutter={10} >
+                <Tabs.TabPane tab="Basic Info" key="tab1" >
+                  <div>this is it </div>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Files" key="tab2">
+                  <div >this is it 2</div>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Mutual Action Plan" key="tab3">
+                  <div >this is it 2</div>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Contact" key="tab4">
+                  <div >this is it 2</div>
+                </Tabs.TabPane>
+              </Tabs>
             </div>
           </div>
         </div>
